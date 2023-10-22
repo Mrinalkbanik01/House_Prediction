ab86c7b64f7f353d93fa5acae71aa4b89d76cd21
Mrinalkbanik01 <manukbanik30@gmail.com>
2023-10-22 20:21:21 +0530

    Backend fully created Minor changes left

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify , render_template , redirect , url_for , request
 import util
 
 app = Flask(__name__)
@@ -11,20 +11,29 @@ app = Flask(__name__)
 #     response.headers.add('Access-Control-Allow-Origin', '*')
 
 #     return response
+@app.route('/')
+def welcome():
+    prediction = request.args.get("prediction" , "")
+    return render_template("index.html" , prediction=prediction)
 
-@app.route('/predict_home_price', methods=['GET', 'POST'])
+@app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
-    total_sqft = float(request.form['total_sqft'])
-    location = request.form['location']
-    bhk = int(request.form['bhk'])
-    bath = int(request.form['bath'])
+    try:
+        total_sqft = float(request.form['total_sqft'])
+        location = request.form['location']
+        bhk = int(request.form['bhk'])
+        bath = int(request.form['bath'])
 
-    response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
+        estimated_price = util.get_estimated_price(location, total_sqft, bhk, bath)
+        print(estimated_price)
+        estimated_price = str(estimated_price)
+        return redirect(url_for("welcome", prediction=estimated_price))
+    except Exception as e:
+        # Print the error message for debugging
+        print(f"An error occurred: {str(e)}")
 
-    return response
+        # Return an error response or redirect to an error page
+        return "An error occurred while processing the request."
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
