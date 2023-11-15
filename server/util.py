ab368643aaada480054211576e75a6cc5e7c8a79
Mrinalkbanik01 <manukbanik30@gmail.com>
2023-11-15 20:58:52 +0530

    Final Changes

--- server/util.py
+++ server/util.py
@@ -6,15 +6,14 @@ __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location,total_sqft,bhk,bath):
+def get_estimated_price(location,sqft,bhk,bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
         loc_index = -1
 
     x = np.zeros(len(__data_columns))
-    print(x)
-    x[0] = total_sqft
+    x[0] = sqft
     x[1] = bath
     x[2] = bhk
     if loc_index>=0:
@@ -28,13 +27,13 @@ def load_saved_artifacts():
     global  __data_columns
     global __locations
 
-    with open("C:/Users/Mrinal/Desktop/ML_Notes/House_Prediction_Project/Model/columns.json", "r") as f:
+    with open("./artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
 
     global __model
     if __model is None:
-        with open('C:/Users/Mrinal/Desktop/ML_Notes/House_Prediction_Project/Model/banglore_home_prices_model.pickle', 'rb') as f:
+        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
             __model = pickle.load(f)
     print("loading saved artifacts...done")
 
